108cb736809bea2dddc0aa31d88c7b9cdd7414f5
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-12-01 14:00:49 +0700

    remove comment

--- resources/views/layout/footer_script.blade.php
+++ resources/views/layout/footer_script.blade.php
@@ -2,7 +2,6 @@
 <script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
 <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <!-- apps -->
-<!-- apps -->
 <script src="{{ asset('dist/js/app-style-switcher.js') }}"></script>
 <script src="{{ asset('dist/js/feather.min.js') }}"></script>
 <script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
