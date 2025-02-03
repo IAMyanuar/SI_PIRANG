f9a5a57341045ddae9c50a3c55b0df5dd085dd1d
IAMyanuar <ilhamyanuar27@gmail.com>
2025-02-03 22:38:22 +0700

    Memperbaiki pada halaman Kalender

--- resources/views/admin/kalender.blade.php
+++ resources/views/admin/kalender.blade.php
@@ -16,7 +16,7 @@
         <div class="page-breadcrumb">
             <div class="row">
                 <div class="col-7 align-self-center">
-                    <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">Calendar</h4>
+                    <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">Kalendar</h4>
                     <div class="d-flex align-items-center">
                         {{-- <nav aria-label="breadcrumb">
                             <ol class="breadcrumb m-0 p-0">
